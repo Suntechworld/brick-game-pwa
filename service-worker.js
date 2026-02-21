9ff38b8d60fab497b5f724a968feef35de254880
Suntechworld <suntechworldglobalnigeria@gmail.com>
2026-02-21 12:58:15 +0100

    Creating service-worker.js with offline support and caching functionality

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/images/logo.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Cache hit - return response from cache
+            if (response) {
+                return response;
+            }
+            // Fallback to network
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['my-cache'];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
